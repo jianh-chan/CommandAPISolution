ea0d2864d9e9f1ab7349d8c3ea8bd094b196a4d3
Jian Hao Chan <jianhaochan@gmail.com>
2020-11-17 21:35:47 +0000

    Added POST endpoint to CommandsController

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -32,7 +32,7 @@ namespace CommandAPI.Controllers
         }
 
         //GET api/commands/5
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name="GetCommandById")]
         public ActionResult<CommandReadDto> GetCommandById(int id)
         {
             var commandItem = _repository.GetCommandById(id);
@@ -42,5 +42,19 @@ namespace CommandAPI.Controllers
             }
             return Ok(_mapper.Map<CommandReadDto>(commandItem));
         }
+
+        //POST api/commands
+        [HttpPost]
+        public ActionResult<CommandReadDto> CreateCommand(CommandCreateDto commandCreateDto)
+        {
+            var commandModel = _mapper.Map<Command>(commandCreateDto);
+            _repository.CreateCommand(commandModel);
+            _repository.SaveChanges();
+
+            var commandReadDto = _mapper.Map<CommandReadDto>(commandModel);
+
+            return CreatedAtRoute(nameof(GetCommandById), new { Id = commandReadDto.Id }, commandReadDto);
+        }
+
     }
 }
--- Profiles/CommandsProfile.cs
+++ Profiles/CommandsProfile.cs
@@ -8,7 +8,9 @@ namespace CommandAPI.Profiles
     {
         public CommandsProfile()
         {
+            // Source -> Target
             CreateMap<Command, CommandReadDto>();
+            CreateMap<CommandCreateDto, Command>();
         }
     }
 }
